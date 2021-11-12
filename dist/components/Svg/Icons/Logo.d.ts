4d20144c70f860c30bc4fd33f7fb392c9fa69af8
jj19100 <1910026791@qq.com>
2021-11-12 11:20:38 +0800

    修改logo

--- dist/components/Svg/Icons/Logo.d.ts
+++ dist/components/Svg/Icons/Logo.d.ts
@@ -1,7 +1,4 @@
 import React from "react";
 import { SvgProps } from "../types";
-interface LogoProps extends SvgProps {
-    isDark?: boolean;
-}
-declare const Icon: React.FC<LogoProps>;
+declare const Icon: React.FC<SvgProps>;
 export default Icon;
