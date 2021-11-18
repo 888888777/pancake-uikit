0511b2223597c5f0a3862724d689442a5c9e49fc
jj19100 <1910026791@qq.com>
2021-11-18 17:44:25 +0800

    update footer iconLinks

--- dist/components/Footer/config.d.ts
+++ dist/components/Footer/config.d.ts
@@ -1,13 +1,9 @@
 import { Language } from "../LangSelector/types";
 import { FooterLinkType } from "./types";
 export declare const footerLinks: FooterLinkType[];
-export declare const socials: ({
+export declare const socials: {
     label: string;
     icon: string;
     href: string;
-} | {
-    label: string;
-    icon: string;
-    href?: undefined;
-})[];
+}[];
 export declare const langs: Language[];
