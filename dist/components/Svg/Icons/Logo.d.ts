15dd3f26552094235cdb817149981680e1b936c7
yingjiechen <1910026791@ecoball.org>
2021-11-19 18:04:58 +0800

    updata logo with mobile

--- dist/components/Svg/Icons/Logo.d.ts
+++ dist/components/Svg/Icons/Logo.d.ts
@@ -3,5 +3,5 @@ import { SvgProps } from "../types";
 interface LogoProps extends SvgProps {
     isDark?: boolean;
 }
-declare const Icon: React.FC<LogoProps>;
-export default Icon;
+declare const _default: React.NamedExoticComponent<LogoProps>;
+export default _default;
